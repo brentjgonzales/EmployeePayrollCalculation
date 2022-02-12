86a163a695428467c7b577459e508fed5cd77201
Brent Gonzales <brentjgonzales@gmail.com>
2022-02-12 10:31:32 -0500

    Added functionality to add dependents on the Create Employee page.

--- EmployeePayrollCalculation/client-app/src/components/CreateEmployee.tsx
+++ EmployeePayrollCalculation/client-app/src/components/CreateEmployee.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import {useForm} from "react-hook-form";
+import React, {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 interface ILocalState {
-    firstName: string,
-    lastName: string,
+    name: string,
+    dependents: IDependent[]
+}
 
+interface IDependent {
+    id: string,
+    name: string
 }
 
 const CreateEmployee = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data: any) => console.log(data);
+    const [employee, setEmployee] = useState<ILocalState>({
+        name: '',
+        dependents: [] as IDependent[]
+    });
+
+    const addDependent = (e: any) => {
+        setEmployee({
+            ...employee,
+            dependents: [...employee.dependents, {
+                id: uuidv4(),
+                name: ''
+            }]
+        });
+    };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("firstName", { required: true, maxLength: 20 })} />
-            <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-            <input type="number" {...register("age", { min: 18, max: 99 })} />
-            <input type="submit" />
-        </form>
+
+        <>
+            <form onSubmit={() => {
+            }}>
+                <h1>Create Employee</h1>
+                <h2>Employee</h2>
+                <div className='mb-3'>
+                    <label htmlFor='name' className='form-label'>Name</label>
+                    <input type='text' className='form-control' name='name'/>
+                    {/*<input type='text' className='form-control' {...register('name', { required: true, maxLength: 100 })} />*/}
+                    {/*<span className='text-danger'>{errors.name?.type === 'required' && 'Name is required.'}</span>*/}
+                    {/*<span className='text-danger'>{errors.name?.type === 'maxLength' && 'Name cannot be more than 100 characters.'}</span>*/}
+                </div>
+                {employee.dependents.map((dependent, index) => {
+                    return (
+                        <div key={dependent.id} className='mb-3'>
+                            <label htmlFor={'dependent-name-' + dependent.id} className='form-label'>Dependent
+                                #{index + 1}</label>
+                            <input type='text' className='form-control' name={'dependent-name-' + dependent.id}/>
+                        </div>
+                    );
+                })}
+                <div className='mb-3'>
+                    <button type='button' className='btn btn-link' onClick={(e) => addDependent(e)}>+ Add Dependent
+                    </button>
+                </div>
+                <input type='submit' className='btn btn-success'/>
+            </form>
+        </>
     );
 }
 
