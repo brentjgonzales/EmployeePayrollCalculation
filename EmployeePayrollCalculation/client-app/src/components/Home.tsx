78d586ca4999bd0de407d93c552bf60805b7fc90
Brent Gonzales <brentjgonzales@gmail.com>
2022-02-14 20:46:06 -0500

    forgot to check in Home.tsx

---
+++ EmployeePayrollCalculation/client-app/src/components/Home.tsx
@@ -0,0 +1,23 @@
+﻿import {Link} from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h2 className="mb-4">Home</h2>
+            <p>Please review the actions you may take below.</p>
+            <ul>
+                <li>
+                    <Link to="/configuration">Configuration</Link>
+                </li>
+                <li>
+                    <Link to="/manage-employees">Manage Employees</Link>
+                </li>
+                <li>
+                    <Link to="/create-employee">Create Employee</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
